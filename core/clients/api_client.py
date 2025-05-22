37ccd656b35c9137e1fc3671979d917399aa1248
zajkovdd <zzigqushka@gmail.com>
2025-05-23 00:30:49 +0300

    Add ping and auth test. Add config and endpoints

--- core/clients/api_client.py
+++ core/clients/api_client.py
@@ -2,6 +2,9 @@ import requests
 import os
 from dotenv import load_dotenv
 from core.settings.environment import Environment
+from core.clients.endpoints import Endpoints
+from core.settings.config import Users, Timeouts
+import allure
 
 load_dotenv()
 
@@ -14,7 +17,8 @@ class APIClient:
             raise ValueError(f'Unsupported environment value: {environment_str}')
 
         self.base_url = self.get_base_url(environment)
-        self.headers = {
+        self.session = requests.Session()
+        self.session.headers = {
             'Content-Type': 'application/json'
         }
 
@@ -39,3 +43,27 @@ class APIClient:
         if status_code:
             assert response.status_code == status_code
         return response.json()
+
+    def ping(self):
+        with allure.step('Ping API client'):
+            url = f'{self.base_url}{Endpoints.PING_ENDPOINT}'
+            response = self.session.get(url)
+            response.raise_for_status()
+        with allure.step('Assert status code'):
+            assert response.status_code == 201, f'Expected status code 201 but got {response.status_code}'
+        return response.status_code
+
+    def auth(self):
+        with allure.step('Getting authenticate'):
+            url = f'{self.base_url}{Endpoints.AUTH_ENDPOINT}'
+            payload = {'username': Users.USERNAME, 'password': Users.PASSWORD}
+            response = self.session.post(url, json=payload, timeout=Timeouts.TIMEOUT)
+            response.raise_for_status()
+        with allure.step('Checking status code'):
+            assert response.status_code == 200, f'Expected status code 200 but got {response.status_code}'
+        token = response.json().get('token')
+        with allure.step('Updating header with authorization'):
+            self.session.headers.update({'Authorization' : f'Bearer {token}'})
+
+    def get_booking_by_id(self, booking_id):
+        pass
---
+++ core/clients/endpoints.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Endpoints(Enum):
+    PING_ENDPOINT = '/ping'
+    AUTH_ENDPOINT = '/auth'
+    BOOKING_ENDPOINT = '/booking'
