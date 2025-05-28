b94498f24c89eaabf9baa67ec84855908691d413
zajkovdd <zzigqushka@gmail.com>
2025-05-28 15:37:32 +0300

    Replaced test fot create booking

---
+++ tests/test_create_booking.py
@@ -0,0 +1,9 @@
+import allure
+
+
+@allure.feature('Test Create Booking')
+@allure.story('Test data after create booking')
+def test_data_after_create_booking(api_client, generate_random_booking_data):
+    booking_data = generate_random_booking_data
+    response = api_client.create_booking(booking_data=booking_data)
+    assert response.json == booking_data, f'Expected similar response but got {response.json}'
